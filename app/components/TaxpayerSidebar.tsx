8eaf39fb7691a72d0868a2668b16637ced5f632e
mtdlab001-star <mtdlab001@gmail.com>
2026-08-19 18:12:19 +0100

    Add reliefs navigation

--- app/components/TaxpayerSidebar.tsx
+++ app/components/TaxpayerSidebar.tsx
@@ -30,6 +30,7 @@ export default async function TaxpayerSidebar({taxpayerId,active}:Props){
    {item('end-of-year','End of Year',`/taxpayers/${id}/end-of-year`,true)}
    {item('adjustments','Annual Adjustments & Losses',`/taxpayers/${id}/end-of-year/adjustments`,true)}
    {item('tax-liability','Tax Liability Adjustments',`/taxpayers/${id}/end-of-year/tax-liability`,true)}
+   {item('reliefs','Reliefs & Deductions',`/taxpayers/${id}/end-of-year/reliefs`,true)}
    {item('history','Submission History',`/taxpayers/${id}/quarterly/history`,true)}
   </div>
   <div className="operator">Operated by Glomaxel IT Service</div>
