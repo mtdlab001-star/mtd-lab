d648b6484afa014fe56c0ba791ea086aeb0dccca
mtdlab001-star <mtdlab001@gmail.com>
2026-08-19 18:13:28 +0100

    Add other income navigation

--- app/components/TaxpayerSidebar.tsx
+++ app/components/TaxpayerSidebar.tsx
@@ -31,6 +31,7 @@ export default async function TaxpayerSidebar({taxpayerId,active}:Props){
    {item('adjustments','Annual Adjustments & Losses',`/taxpayers/${id}/end-of-year/adjustments`,true)}
    {item('tax-liability','Tax Liability Adjustments',`/taxpayers/${id}/end-of-year/tax-liability`,true)}
    {item('reliefs','Reliefs & Deductions',`/taxpayers/${id}/end-of-year/reliefs`,true)}
+   {item('other-income','Other SA Income',`/taxpayers/${id}/end-of-year/other-income`,true)}
    {item('history','Submission History',`/taxpayers/${id}/quarterly/history`,true)}
   </div>
   <div className="operator">Operated by Glomaxel IT Service</div>
